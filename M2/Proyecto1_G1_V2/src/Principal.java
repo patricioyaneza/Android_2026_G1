6d975ee79160ae9ed836706ccf8f9586572a9414
Patricio <patricio.yanez@fundacionluterana.cl>
2026-05-18 20:53:39 -0400

    Update

--- M2/Proyecto1_G1_V2/src/Principal.java
+++ M2/Proyecto1_G1_V2/src/Principal.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Principal { 
 
@@ -17,6 +18,8 @@ public class Principal {
 		int numero2; // se crea la variable pero no se le asigna un valor
 		numero2 = 200; // se asigna un valor a la variable numero2
 		
+		int valor = 999999999;
+		long valor2 = 9999999999999999L; // se debe agregar la letra L al final del número para indicar que es un valor de tipo long
 		// error de compilación, no se puede asignar un valor de tipo String a una variable de tipo int
 		// numero2 = "dfasdf"; 
 		
@@ -25,7 +28,19 @@ public class Principal {
 		System.out.println(nombre);
 		System.out.println(numero2);
 		
+		float nota = 6.5f; 
+		double nota2 = 6.5; 
+	
+// Leer teclado
+		Scanner leerTeclado = new Scanner(System.in);
+		
+		System.out.print("Ingrese su nombre: ");
+		String nombreUsuario = leerTeclado.nextLine(); 
+		System.out.println("Hola " + nombreUsuario);
+		
+		// ejercicio 2: pedir apellido y edad
+		
+		
+		
 	}
-
-
 }
